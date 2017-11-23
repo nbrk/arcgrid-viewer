2463e6ee3ca164563a4e814984147583d9a3f955
Nikolay Burkov <nbrk@tenker.localdomain>
2017-11-23 06:27:09 +0300

    use System.Clock to profile the parser and rangeOfVAT

--- src/DataArcGrid.hs
+++ src/DataArcGrid.hs
@@ -4,17 +4,22 @@ import Data.List
 import Data.Maybe
 import ArcGrid
 import Text.Printf
+import System.Clock
 
 import Types
 
 dataFromArcGridFile :: FilePath -> IO Data
 dataFromArcGridFile f = do
+  profstart <- getTime Monotonic
   ag <- arcGridFromFile f
+  profend <- getTime Monotonic
 
   let rng = rangeOfVAT (nodata_value ag) (vat ag)
 
   printf "Parsed %dx%d ArcGrid VAT (cell size %f). Value range is %s, ignoring %s\n"
     (ncols ag) (nrows ag) (cellsize ag) (show rng) (show (nodata_value ag))
+  printf "Parser and rangeOfVAT took %f msecs.\n" $
+    (fromIntegral (nsec profend - nsec profstart)) / (1000000 :: Float)
 
   return $ Data
     { _dataValues = vat ag
